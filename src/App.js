4b4f84bd674fedf6169bdc5dd52acdc784f1678c
Bryan <windsor.bryan@gmail.com>
2019-01-05 01:57:44 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "tachyons";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App h-100 ma0">
+        <div className="Navbar br2 shadow-5 pa2 ma0 min-h border">Navbar</div>
+        <div className="Board center h-100 pa2 border">
+          <div className="boardLeft w-60 center br2 shadow-5 pa2 border">
+            Slides
+          </div>
+          <div className="boardRight w-40 center br2 shadow-5 pa2 border">
+            <div className="Student h-50 br2 shadow-5 pa2 w-100 center border">
+              Student
+              <div />
+            </div>
+            <div className="boardBottomRight w-100 h-50 br2 shadow-5 center border">
+              <div className="Toolbar w-20 br2 shadow-5 center border">
+                Toolbar
+              </div>
+              <div className="Teacher w-80 br2 shadow-5 center border">
+                Teacher
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
